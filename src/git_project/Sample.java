308f322fa601deec126f66941326eec10f6fdf57
ELCOT <ELCOT@Lenovo>
2024-06-18 23:36:45 +0530

    Second commit

--- src/git_project/Sample.java
+++ src/git_project/Sample.java
@@ -2,6 +2,6 @@ package git_project;
 
 public class Sample {
 	public static void main(String[] args) {
-		System.out.println("Git and Github");
+		System.out.println("Welcome to Git and Github");
 	}
 }
